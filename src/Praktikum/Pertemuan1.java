1ad537867ea6a0a12e46ae729d71c7fa6d3e7c36
Musixient <m.arief.furqany@gmail.com>
2023-03-30 22:13:16 +0700

    pembuatan kode

--- src/Praktikum/Pertemuan1.java
+++ src/Praktikum/Pertemuan1.java
@@ -3,8 +3,23 @@ package Praktikum;
 public class Pertemuan1 {
 
     public static void main(String[] args) {
-        
-       System.out.println("arief"); 
-    }
+    
+    System.out.println("Menghitung luas balok"); 
+    System.out.println("");
+    System.out.println("rumus luas balok adalah 'panjang x lebar x tinggi'");
+
+    double panjang, lebar, tinggi, luas;
+
+    // Ddeklarai nilai variable
+    panjang = 7.5;
+    lebar = 2.2;
+    tinggi = 4;
+
+    //operasi luas
+    luas = panjang * lebar * tinggi;
+
+    System.out.println("panjang = " + panjang +" * lebar = " + lebar + " * tinggi = " + tinggi );
+    System.out.println(luas);
+   }
     
 }
